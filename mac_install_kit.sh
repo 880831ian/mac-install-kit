0c6f3cadf25afc8c193ca9398db11028524dad33
ian_zhuang <880831ian@gmail.com>
2022-09-21 12:16:56 +0800

    新增 zsh 設定 (只顯示帳號名稱、只顯示當前路徑)

--- mac_install_kit.sh
+++ mac_install_kit.sh
@@ -3,13 +3,14 @@
 #=========================================
 # 參數設定
 
-brew_array=("sshpass" "wget" "nginx" "mysql" "php@8.0" "phpmyadmin" "minikube" "kubernetes-cli" "helm")
+brew_array=("sshpass" "minikube" "kubernetes-cli" "helm" "snyk")
 
 #=========================================
 # 腳本設定
 
 nowtime=$(date '+%Y/%m/%d %H:%M:%S')
-var=0;num=0;
+var=0
+num=0
 
 #=========================================
 # 顏色設定
@@ -30,35 +31,47 @@ echo "腳本開始時間 ${nowtime}"
 
 # 安裝 Homebrew
 
-var=`expr $var '+' 1`; num=`expr $num '+' 1`;
+var=$(expr $var '+' 1)
+num=$(expr $num '+' 1)
 brew -v 1>/dev/null
-if [ "$?" = "0" ];then
-echo "${num} _ 已安裝 Homebrew:                  [${yellow}Warning${white}]"
-var=`expr $var '-' 1`;
+if [ "$?" = "0" ]; then
+	echo "${num} _ 安裝 Homebrew:                  [${yellow}已安裝${white}]"
+	var=$(expr $var '-' 1)
 else
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-echo "${num} _ 安裝 Homebrew:                    [${green}Success${white}]"
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	echo "${num} _ 安裝 Homebrew:                    [${green}安裝成功${white}]"
 fi
 
 # 安裝 Homebrew - 套件(套件列表請參考參數設定 brew_array)
 
-for kit in ${brew_array[@]}
-do
-	var=`expr $var '+' 1`; num=`expr $num '+' 1`;
+for kit in ${brew_array[@]}; do
+	var=$(expr $var '+' 1)
+	num=$(expr $num '+' 1)
 	brew list | grep $kit 1>/dev/null
-	if [ "$?" = "0" ];then
-	echo "${num} _ 已安裝 Homebrew ($kit):   [${yellow}Warning${white}]"
-	var=`expr $var '-' 1`;
+	if [ "$?" = "0" ]; then
+		echo "${num} _ 安裝 Homebrew ($kit):   	  [${yellow}已安裝${white}]"
+		var=$(expr $var '-' 1)
 	else
-	brew install $kit 1>/dev/null
-	echo "${num} _ 安裝 Homebrew ($kit):     [${green}Success${white}]"
+		brew install $kit 1>/dev/null
+		echo "${num} _ 安裝 Homebrew ($kit):          [${green}安裝成功${white}]"
 	fi
 done
 
+# 設定 zsh 主題 (agnoster) 只顯示帳號名稱
+var=$(expr $var '+' 1)
+num=$(expr $num '+' 1)
+sed -i -e 's/%n@%m/%n/g' $ZSH/themes/agnoster.zsh-theme
+echo "${num} _ 設定 zsh 主題 (只顯示帳號名稱):          [${green}設定成功${white}]"
+
+# 設定 zsh 主題 (agnoster) 只顯示當前路徑
+var=$(expr $var '+' 1)
+num=$(expr $num '+' 1)
+sed -i -e 's/%~/%c/g' $ZSH/themes/agnoster.zsh-theme
+echo "${num} _ 設定 zsh 主題 (只顯示當前路徑):          [${green}設定成功${white}]"
 
 #=========================================
-
 # 輸出統計
 
 echo "\n=====================================統計輸出==================================="
-echo "${blue}安裝套件成功數 / 安裝套件總數 / 成功率：( ${green}$var${white} ${blue}/ $num / )"
+success_rate=$(echo "scale=2; $var/$num*100" | bc -l)
+echo "${blue}安裝套件成功數 / 安裝套件總數 / 成功率：( ${green}$var${white} ${blue}/ $num / ${red}$success_rate%${white} )"
