ed7d35f21ed4dd76539810e112e4146725a65bf4
ian_zhuang <880831ian@gmail.com>
2022-09-22 12:17:39 +0800

    整理腳本

--- mac_install_kit.sh
+++ mac_install_kit.sh
@@ -2,8 +2,8 @@
 
 #=========================================
 # 參數設定
-
-brew_array=("sshpass" "minikube" "kubernetes-cli" "helm" "snyk")
+brew_tap_array=("snyk/tap") # 安裝不在 homebrew 的第三方套件
+brew_array=("sshpass" "minikube" "kubernetes-cli" "kustomize" "helm" "snyk")
 
 #=========================================
 # 腳本設定
@@ -35,12 +35,27 @@ var=$(expr $var '+' 1)
 num=$(expr $num '+' 1)
 brew -v 1>/dev/null
 if [ "$?" = "0" ]; then
-	echo "${num} _ 安裝 Homebrew:                  [${yellow}已安裝${white}]"
+	echo "${num} _ 安裝 Homebrew: [${yellow}已安裝${white}]"
 	var=$(expr $var '-' 1)
 else
 	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-	echo "${num} _ 安裝 Homebrew:                    [${green}安裝成功${white}]"
+	echo "${num} _ 安裝 Homebrew: [${green}安裝成功${white}]"
 fi
+
+# 安裝 Homebrew - 第三方套件(套件列表請參考參數設定 brew_tap_array)
+
+for kit in ${brew_tap_array[@]}; do
+	var=$(expr $var '+' 1)
+	num=$(expr $num '+' 1)
+	brew tap | grep $kit 1>/dev/null
+	if [ "$?" = "0" ]; then
+		echo "${num} _ 安裝 Homebrew_tap ($kit): [${yellow}已安裝${white}]"
+		var=$(expr $var '-' 1)
+	else
+		brew install $kit 1>/dev/null
+		echo "${num} _ 安裝 Homebrew_tap ($kit): [${green}安裝成功${white}]"
+	fi
+done
 
 # 安裝 Homebrew - 套件(套件列表請參考參數設定 brew_array)
 
@@ -49,11 +64,11 @@ for kit in ${brew_array[@]}; do
 	num=$(expr $num '+' 1)
 	brew list | grep $kit 1>/dev/null
 	if [ "$?" = "0" ]; then
-		echo "${num} _ 安裝 Homebrew ($kit):   	  [${yellow}已安裝${white}]"
+		echo "${num} _ 安裝 Homebrew ($kit): [${yellow}已安裝${white}]"
 		var=$(expr $var '-' 1)
 	else
 		brew install $kit 1>/dev/null
-		echo "${num} _ 安裝 Homebrew ($kit):          [${green}安裝成功${white}]"
+		echo "${num} _ 安裝 Homebrew ($kit): [${green}安裝成功${white}]"
 	fi
 done
 
@@ -61,13 +76,13 @@ done
 var=$(expr $var '+' 1)
 num=$(expr $num '+' 1)
 sed -i -e 's/%n@%m/%n/g' $ZSH/themes/agnoster.zsh-theme
-echo "${num} _ 設定 zsh 主題 (只顯示帳號名稱):          [${green}設定成功${white}]"
+echo "${num} _ 設定 zsh 主題 (只顯示帳號名稱): [${green}設定成功${white}]"
 
 # 設定 zsh 主題 (agnoster) 只顯示當前路徑
 var=$(expr $var '+' 1)
 num=$(expr $num '+' 1)
 sed -i -e 's/%~/%c/g' $ZSH/themes/agnoster.zsh-theme
-echo "${num} _ 設定 zsh 主題 (只顯示當前路徑):          [${green}設定成功${white}]"
+echo "${num} _ 設定 zsh 主題 (只顯示當前路徑): [${green}設定成功${white}]"
 
 #=========================================
 # 輸出統計
